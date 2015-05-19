59a16d60fb8d5d398cb09fc52df8ac743e57266c
sergej <sergej@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2015-05-19 11:57:42 +0000

    [385] Review fix: hopefully my comments won't make poor guy's life harder who will be reading my code and now comments
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@227 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- example/com/pensio/api/Example02_GiftCards.java
+++ example/com/pensio/api/Example02_GiftCards.java
@@ -22,14 +22,20 @@ public class Example02_GiftCards
 		PensioProcessorAPI processorApi = new PensioProcessorAPI(gatewayUrl, username, password);
 		PensioMerchantAPI merchantApi = new PensioMerchantAPI(gatewayUrl, username, password);
 		
+		//Setting parameters of payment request (order id, terminal, amount)
         PaymentRequest request = new PaymentRequest("giftcard_"+String.valueOf(Math.round(Math.random()*100000)),"Test GiftCard Terminal", Amount.get(10.00, Currency.EUR));
+		//Creating payment request to link incoming gift card payments
         PaymentRequestResponse response = merchantApi.createPaymentRequest(request);
 
+        //Preparing reservation request to make gift card payment
 		PaymentReservationRequest reservationRequest = new PaymentReservationRequest(response.getPaymentRequestId());
+		//Setting gift card and gift card provider
 		reservationRequest.setGiftCard(new GiftCard().setAccountIdentifier("88880000010000").setProvider("test"));
 
+		//Sending request to the gateway
 		APIResponse initiateResponse = processorApi.initiateGiftCardPayment(reservationRequest);
 
+		//Checking the result
 		if("Success".equals(initiateResponse.getBody().getResult()))
 		{
 			System.out.println("Paid successfully!");
