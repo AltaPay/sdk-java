280c4b18751f819abd010c30cc44d1b42720a3a8
rolandas <rolandas@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2015-06-25 14:40:10 +0000

    [692] Add missing values to AuthType and a method to convert deprecated auth types. Add RecurringPreauthTests.
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@236 3f9e65d7-54e3-4c48-a158-52a0885c5079

--- src/com/pensio/api/request/AuthType.java
+++ src/com/pensio/api/request/AuthType.java
@@ -2,5 +2,23 @@ package com.pensio.api.request;
 
 public enum AuthType
 {
-	payment, paymentAndCapture, subscription, subscriptionAndCharge, verifyCard, credit
+	payment, paymentAndCapture, subscription, subscriptionAndCharge, subscriptionAndReserve, subscription_payment, verifyCard, credit;
+	
+	//This is in order to recreate php tests, not sure if this the way to do it 
+	public static AuthType supportDeprecatedAuthType(String type) {
+		if(type=="recurring")
+		{
+			return AuthType.subscription;
+		}
+		if(type=="recurringAndCapture")
+		{
+			return AuthType.subscriptionAndCharge;
+		}
+		if(type=="recurring_payment")
+		{
+			return AuthType.subscription_payment;
+		}
+		return null;
+	}
+
 }
