98ebba8e2d45358d165a5814428221c1cac6a6b0
emerson <emerson@3f9e65d7-54e3-4c48-a158-52a0885c5079>
2017-07-06 09:25:40 +0000

    [p77] Unit tests about the reading of the API response xml elements.
    git-svn-id: svn+ssh://office-admin/home/subversion/PensioClientAPI/trunk@293 3f9e65d7-54e3-4c48-a158-52a0885c5079

---
+++ test/unit/com/pensio/api/MerchantApi_ParsePostBackXmlResponseTests.java
@@ -0,0 +1,54 @@
+package com.pensio.api;
+
+import com.pensio.api.PensioMerchantAPI;
+import com.pensio.api.generated.APIResponse;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.Paths.get;
+
+/**
+ * Created by emerson on 7/5/17.
+ */
+public class MerchantApi_ParsePostBackXmlResponseTests
+{
+	private PensioMerchantAPI api;
+
+	@Before
+	public void setUp() throws Exception
+	{
+		api = new PensioMerchantAPI("url", "username", "password");
+	}
+
+	@Test
+	public void ParsePostBackXmlResponse_ReadCardHolderMessageMustBeShown() throws PensioAPIException, IOException
+	{
+		String xmlResponse = readFile("test/unit/com/pensio/api/txt/CardHolderMessageMustBeShownFalse.xml");
+		APIResponse response = api.parsePostBackXMLParameter(xmlResponse);
+		Assert.assertFalse(response.getBody().isCardHolderMessageMustBeShown());
+
+		xmlResponse = readFile("test/unit/com/pensio/api/txt/CardHolderMessageMustBeShownTrue.xml");
+		response = api.parsePostBackXMLParameter(xmlResponse);
+		Assert.assertTrue(response.getBody().isCardHolderMessageMustBeShown());
+	}
+
+	private String readFile(String file) throws IOException
+	{
+		return new String(readAllBytes(get((file))));
+	}
+
+	@Test
+	public void ParsePostBackXmlResponse_ReadReasonCode() throws PensioAPIException, IOException
+	{
+		String xmlResponse = readFile("test/unit/com/pensio/api/txt/ReasonCode.xml");
+		APIResponse response = api.parsePostBackXMLParameter(xmlResponse);
+		Assert.assertEquals("NONE", response.getBody().getTransactions().getTransaction().get(0).getReasonCode());
+
+	}
+
+}
