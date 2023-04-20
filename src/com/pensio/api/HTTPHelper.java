f5935c966c2c14e1381b2eb39cb6c4b11403b5f1
Ovidiu Balea <ovidiub@altapay.com>
2023-04-20 09:53:58 +0300

    COREP-1709 add doGet method in HTTPHelper

--- src/com/pensio/api/HTTPHelper.java
+++ src/com/pensio/api/HTTPHelper.java
@@ -7,6 +7,7 @@ import java.io.InputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -68,6 +69,43 @@ public class HTTPHelper {
 		printout.writeBytes(content);
 		printout.flush();
 		printout.close();
+
+		// Get response data.
+		return new DataInputStream(urlConn.getInputStream());
+	}
+	
+	public InputStream doGet(String urlString, Map<String, String> getVars, String username, String password, String sdkVersion) throws IOException {
+		
+		String query = "";
+		boolean isFirst = true;
+		for(Entry<String, String> e : getVars.entrySet())
+		{
+			query += (isFirst ? "?" : "&") + e.getKey() + "=" + URLEncoder.encode(e.getValue(), StandardCharsets.UTF_8.name());
+			isFirst = false;
+		}
+		
+		// URL of CGI-Bin script.
+		URL url = new URL(urlString + query);
+
+		// URL connection channel.
+		URLConnection urlConn = url.openConnection();
+	
+
+		// Let the RTS know that we want to do output.
+		urlConn.setDoOutput(true);
+
+		// No caching, we want the real thing.
+		urlConn.setUseCaches(false);
+
+		urlConn.setRequestProperty("Accept-Charset", StandardCharsets.UTF_8.name());
+		urlConn.setRequestProperty("x-altapay-client-version", sdkVersion);
+		urlConn.setRequestProperty("User-Agent", sdkVersion);
+
+		if(username != null)
+		{
+			String encoded = new String(Base64.encodeBase64((username+":"+password).getBytes()));
+			urlConn.setRequestProperty("Authorization", "Basic "+encoded);
+		}
 
 		// Get response data.
 		return new DataInputStream(urlConn.getInputStream());
