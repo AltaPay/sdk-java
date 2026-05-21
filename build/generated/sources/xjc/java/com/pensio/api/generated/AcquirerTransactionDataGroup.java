6e259de433a95057f3407f9bb4a676d05a392ca8
Przemyslaw Poznienski <przemyslawp@altapay.com>
2026-05-21 10:46:39 +0200

    [COREP-5113] fix generated

---
+++ build/generated/sources/xjc/java/com/pensio/api/generated/AcquirerTransactionDataGroup.java
@@ -0,0 +1,104 @@
+
+package com.pensio.api.generated;
+
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.annotation.Generated;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlAttribute;
+import jakarta.xml.bind.annotation.XmlElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Java class for AcquirerTransactionDataGroup complex type.
+ * 
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * 
+ * <pre>
+ * &lt;complexType name="AcquirerTransactionDataGroup"&gt;
+ *   &lt;complexContent&gt;
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType"&gt;
+ *       &lt;sequence&gt;
+ *         &lt;element name="Entry" type="{}AcquirerTransactionDataEntry" maxOccurs="unbounded" minOccurs="0"/&gt;
+ *       &lt;/sequence&gt;
+ *       &lt;attribute name="name" use="required" type="{http://www.w3.org/2001/XMLSchema}string" /&gt;
+ *     &lt;/restriction&gt;
+ *   &lt;/complexContent&gt;
+ * &lt;/complexType&gt;
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "AcquirerTransactionDataGroup", propOrder = {
+    "entry"
+})
+@Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+public class AcquirerTransactionDataGroup {
+
+    @XmlElement(name = "Entry")
+    @Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+    protected List<AcquirerTransactionDataEntry> entry;
+    @XmlAttribute(name = "name", required = true)
+    @Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+    protected String name;
+
+    /**
+     * Gets the value of the entry property.
+     * 
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the Jakarta XML Binding object.
+     * This is why there is not a <CODE>set</CODE> method for the entry property.
+     * 
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getEntry().add(newItem);
+     * </pre>
+     * 
+     * 
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link AcquirerTransactionDataEntry }
+     * 
+     * 
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+    public List<AcquirerTransactionDataEntry> getEntry() {
+        if (entry == null) {
+            entry = new ArrayList<>();
+        }
+        return this.entry;
+    }
+
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    @Generated(value = "com.sun.tools.xjc.Driver", comments = "JAXB RI v3.0.2", date = "2026-05-21T09:05:56+02:00")
+    public void setName(String value) {
+        this.name = value;
+    }
+
+}
